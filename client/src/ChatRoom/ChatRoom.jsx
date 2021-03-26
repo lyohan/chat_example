8ff9befa15f860cc184874de950d445e7761f17c
lyohan <ian_90@likelion.net>
2021-03-26 17:49:53 +0900

    feat: add tailwindcss style chatroom

--- client/src/ChatRoom/ChatRoom.jsx
+++ client/src/ChatRoom/ChatRoom.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 
 import "./ChatRoom.css";
 import useChat from "../useChat";
@@ -6,7 +6,7 @@ import useChat from "../useChat";
 const ChatRoom = (props) => {
   const { roomId } = props.match.params;
   const { messages, sendMessage } = useChat(roomId);
-  const [newMessage, setNewMessage] = React.useState("");
+  const [newMessage, setNewMessage] = useState("");
 
   const handleNewMessageChange = (event) => {
     setNewMessage(event.target.value);
@@ -21,15 +21,19 @@ const ChatRoom = (props) => {
     <div className="chat-room-container">
       <h1 className="room-name">Room: {roomId}</h1>
       <div className="messages-container">
-        <ol className="messages-list">
+        <ol className="flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
           {messages.map((message, i) => (
-            <li
-              key={i}
-              className={`message-item ${
-                message.ownedByCurrentUser ? "my-message" : "received-message"
-              }`}
-            >
-              {message.body}
+            <li className="chat-message" key={i}>
+              <div className={`flex items-end ${!message.ownedByCurrentUser && 'justify-end'}`}>
+                <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
+                  <div>
+                    <span className={`px-4 py-2 rounded-lg inline-block rounded-bl-none ${message.ownedByCurrentUser ? 'bg-gray-300' : 'bg-blue-600'} text-gray-600`}>
+                      {message.body}
+                    </span>
+                  </div>
+                </div>
+                <img src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144" alt="My profile" className="w-6 h-6 rounded-full order-1" />
+              </div>
             </li>
           ))}
         </ol>
